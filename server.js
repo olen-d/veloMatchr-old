f67549b70afd5108d63d10c2e2ad6ec58fb1df63
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-02-19 02:17:15 -0500

    Added materialize, started front end html & content

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
+// Set up the css, js, and images directories
+app.use(express.static("app/public"));
+// app.use(express.static("js"));
+// app.use(express.static("images"));
 
 const apiRoutes = require("./app/routing/apiRoutes")(app);
 const htmlRoutes = require("./app/routing/htmlRoutes")(app);
 
-
-//app.use("/",htmlRoutes);
-
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
