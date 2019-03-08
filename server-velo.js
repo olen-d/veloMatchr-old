ae56b4a41d84a4bf8af1bb193991ec86f5a24ad9
olen-d <olen.daelhousen@outlook.com>
2019-03-08 08:05:17 +0000

    Stash

--- server-velo.js
+++ server-velo.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port =  process.env.PORT || 3000;
+const port =  process.env.PORT || 3060;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
